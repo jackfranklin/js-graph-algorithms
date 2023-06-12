4ce817575374d2f72811d0b3dd6cedcf2135db45
Jack Franklin <jack@jackfranklin.net>
2023-06-12 19:39:56 +0100

    fix some more

--- browser.mjs
+++ browser.mjs
@@ -1,9 +1,9 @@
 import jss from './src/jsgraphs.js'
 
 const {
-  Edge, WeightedDiGraph, Dijkstra, Node, StackNode, Stack, QueueNode, Queue, MinPQ, QuickUnion, IndexMinPQ
+  Edge, WeightedDiGraph, Dijkstra, StackNode, Stack, QueueNode, Queue, MinPQ, QuickUnion, IndexMinPQ
 } = jss
 
 export {
-  Edge, WeightedDiGraph, Dijkstra, Node, StackNode, QueueNode, MinPQ, QuickUnion, IndexMinPQ
+  Edge, WeightedDiGraph, Dijkstra, StackNode, QueueNode, MinPQ, QuickUnion, IndexMinPQ, Stack, Queue
 }
